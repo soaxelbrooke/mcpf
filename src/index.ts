ad294d3be3e638f1dfa454ab652713b95808329e
Stuart Axelbrooke <stuart@axelbrooke.com>
2021-04-04 21:51:34 -0700

    First commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const component = () => {
+  const element = document.createElement('div');
+
+  element.innerHTML = "Hello there.";
+
+  return element;
+};
+
+document.body.appendChild(component());
